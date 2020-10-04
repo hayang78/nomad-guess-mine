e3fb0b6850e6e9d8c9c42562807a670b5a6b7130
차종익 <cha@anytek.com>
2020-10-04 13:57:38 +0900

    Installed PUG

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 
 const PORT = 4000;
-
 const app = express();
+app.set("views engine", "pug");
 
 const handleListening = () =>
   console.log(`Server running: http://localhost:${PORT}`);
