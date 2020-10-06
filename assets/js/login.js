ae016d3c0eabdf2e94895ed97edf7a16e9b7717f
차종익 <cha@anytek.com>
2020-10-07 08:02:01 +0900

    events를 생성하고 render할때 전달하여  frontend에서도 사용할 수 있는 구조 작업

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,8 +8,9 @@ const LOGGED_IN = "loggedIn";
 const nickname = localStorage.getItem(NICKNAME);
 
 const logIn = (nickname) => {
-  const socket = io("/");
-  socket.emit("setNickname", { nickname });
+  // eslint-disable-next-line no-undef
+  window.socket = io("/"); //io는 어떻게 가져오는거지??
+  window.socket.emit(window.events.setNickname, { nickname });
 };
 
 if (nickname === null) {
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import socketIO from "socket.io";
 import logger from "morgan";
 import socketController from "./socketController";
+import events from "./events";
 
 const PORT = 4000;
 const app = express();
@@ -11,7 +12,9 @@ app.set("view engine", "pug");
 app.set("views", join(__dirname, "views")); //views의 경로를 실제경로와 매핑시킨다.
 app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) =>
+  res.render("home", { events: JSON.stringify(events) })
+); //Frontend로 events.js를 전달해준다.
 
 const handleListening = () =>
   console.log(`Server running: http://localhost:${PORT}`);
