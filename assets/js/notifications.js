1277af92d39364588669dcb1de7fd7d01e7217bc
차종익 <cha@anytek.com>
2020-10-08 20:01:10 +0900

    접속, 종료 화면 표시 추가 및 css

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,7 +1,22 @@
 const notifications = document.getElementById("jsNotifications");
 
+const fireNotification = (text, color) => {
+  const notification = document.createElement("div");
+  notification.innerText = text;
+  notification.style.backgroundColor = color;
+  notification.className = "notification";
+  notifications.appendChild(notification);
+};
+
 export const handleNewUser = ({ nickname }) => {
-  console.log(nickname, " just joined");
+  console.log("connect");
+  fireNotification(`${nickname} just joined!`, "rgb(0, 122, 255");
+  //console.log(nickname, " just joined");
+};
+
+export const handleDisconnected = ({ nickname }) => {
+  console.log("disconnect");
+  fireNotification(`${nickname} just left!`, "rgb(255, 149, 0");
 };
 
 if (window.socket) {
--- src/socketController.js
+++ src/socketController.js
@@ -1,10 +1,18 @@
 import events from "./events";
 
 const socketController = (socket) => {
+  const broadcast = (event, data) => socket.broadcast.emit(event, data);
+
   socket.on(events.setNickname, ({ nickname }) => {
     //console.log(nickname);
     socket.nickname = nickname;
-    socket.broadcast.emit(events.newUser, { nickname });
+    broadcast(events.newUser, { nickname });
+  });
+
+  socket.on(events.disconnect, () => {
+    console.log("disconnet");
+    broadcast(events.disconnected, { nickname: socket.nickname });
+    //disconnect 이미 사용중인 이름이라 계속 반복 호출되니 꼭 다른 이름으로 broadcast해야함
   });
 };
 
